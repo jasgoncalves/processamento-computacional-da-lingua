4bc6ab025569eecf06aa66990052a6292cc0e630
Jorge Gonçalves <33223967+jasgoncalves@users.noreply.github.com>
2021-11-16 01:19:48 +0000

    implemented unigram classification

--- Trabalho2/task2_file_creation.py
+++ Trabalho2/task2_file_creation.py
@@ -2,16 +2,16 @@ import pandas as pd
 from pandas import DataFrame
 import numpy as np
 
-DATA_PATH = "Trabalho2\\data\\"
+DATA_PATH = 'Trabalho2\\data\\'
 DELIMITER = '\t'
-EVAL_FILE_NAME = "eval.txt"
+EVAL_FILE_NAME = 'eval'
 EVAL_COLUMNS = ['labels', 'questions', 'answers']
+EXTENSION = '.txt'
 FMT = '% s'
-LABELS_FILE_NAME = "eval-labels.txt"
+LABELS_FILE_NAME = 'eval-labels'
 LABELS_COLUMNS = ['labels']
-QUESTIONS_FILE_NAME = "eval-questions.txt"
+QUESTIONS_FILE_NAME = 'eval-questions'
 QUESTIONS_COLUMNS = ['questions', 'answers']
-
 
 def import_dataset(path : str, columns : str) -> DataFrame:
     return pd.read_csv(path, sep = DELIMITER, names = columns) # '\t' for tab delimiter (.tsv)
@@ -20,6 +20,6 @@ def export_dataset(data_frame : DataFrame, columns : str, file_name : str):
     np.savetxt(fname = file_name, X = data_frame.filter(items=columns).values.tolist(), delimiter = DELIMITER, fmt = FMT)
 
 if __name__ == "__main__":
-    evaluation_dataset = import_dataset(f'{DATA_PATH}{EVAL_FILE_NAME}', EVAL_COLUMNS) # import file eval.txt
-    export_dataset(evaluation_dataset, LABELS_COLUMNS, f'{DATA_PATH}{LABELS_FILE_NAME}') # create file eval-labels.txt
-    export_dataset(evaluation_dataset, QUESTIONS_COLUMNS, f'{DATA_PATH}{QUESTIONS_FILE_NAME}') # create file eval-questions.txt
+    evaluation_dataset = import_dataset(f'{DATA_PATH}{EVAL_FILE_NAME}{EXTENSION}', EVAL_COLUMNS) # import file eval.txt
+    export_dataset(evaluation_dataset, LABELS_COLUMNS, f'{DATA_PATH}{LABELS_FILE_NAME}{EXTENSION}') # create file eval-labels.txt
+    export_dataset(evaluation_dataset, QUESTIONS_COLUMNS, f'{DATA_PATH}{QUESTIONS_FILE_NAME}{EXTENSION}') # create file eval-questions.txt
