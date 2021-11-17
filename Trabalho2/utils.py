72da33e5e1b1c68103bbfdce52fee2f422dc12cf
Diogo Fernandes <diogormsf@gmail.com>
2021-11-17 01:05:37 +0000

    changes to utils.py

--- Trabalho2/utils.py
+++ Trabalho2/utils.py
@@ -2,6 +2,7 @@ import os
 from typing import List
 
 import nltk
+import chardet
 import pandas as pd
 
 PROJECT_PATH = os.path.dirname("Trabalho2")
@@ -11,13 +12,16 @@ OUTPUT_PATH_TASK3 = os.path.join(PROJECT_PATH, "counts2/")
 DELIMITER = '\t'
 EXTENSION = '.txt'
 INITIAL_COLUMNS = ['labels', 'questions', 'answers']
+DATA_COLUMNS = ['questions', 'answers']
 TRAIN_FILE_NAME = "train"
 UNIGRAM_FILE_NAME = 'unigrams_'
 BIGRAM_FILE_NAME = 'bigrams_'
 
 
-def import_dataset(path: str, columns: str) -> pd.DataFrame:
-    return pd.read_csv(path, sep=DELIMITER, names=columns)  # '\t' for tab delimiter (.tsv)
+def import_dataset(path : str, columns : str) -> pd.DataFrame:
+    with open(path, 'rb') as f:
+        enc = chardet.detect(f.read())
+    return pd.read_csv(path, sep = DELIMITER, names = columns, encoding = enc['encoding']) # '\t' for tab delimiter (.tsv)
 
 
 def nltk_ngrams(tokens_list: List[str], ngram_order: int):
