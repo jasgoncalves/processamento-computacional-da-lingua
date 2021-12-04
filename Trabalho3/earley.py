5f3a7da2d70ff7e50f7113a6056aa2f5b5fa6d46
Diogo Fernandes <diogormsf@gmail.com>
2021-12-04 23:27:44 +0000

    Mudada a logica da gramatica para a classe gramatica

--- Trabalho3/earley.py
+++ Trabalho3/earley.py
@@ -1,68 +1,19 @@
 import os
-from typing import List
-
-import chardet
-import pandas as pd
-from pandas import isna
-
-from rule import Rule
 from grammar import Grammar
-from constituent import Constituent
+
 
 PROJECT_PATH = os.path.dirname("Trabalho3")
 GRAMMAR_PATH = os.path.join(PROJECT_PATH, "grammar.txt")
-COLUMNS = ["lhs", "sep", "param0", "param1", "param2"]
 NON_TERMINALS = ["S", "NP", "VP", "PP"]
 TERMINALS = ["Art", "Noun", "Prep", "Verb", "Adj"]
 
 
-def read_grammar(path: str) -> pd.DataFrame:
-    with open(path, 'rb') as f:
-        enc = chardet.detect(f.read())
-    return pd.read_csv(path, sep='\t', names=COLUMNS, encoding=enc['encoding'])
-
-
-def get_constituent(consts: List[Constituent], representation: str) -> Constituent:
-    for const in consts:
-        if const.representation.__eq__(representation):
-            return const
-
-
-def create_lexical_grammar(grammar_df: pd.DataFrame) -> List[Constituent]:
-    term_constituents = []
-    for representation in TERMINALS:
-        curr_const = Constituent(representation, True)
-        term_constituents.append(curr_const)
-
-    for index, row in grammar_df.iterrows():
-        if row.lhs in TERMINALS:
-            get_constituent(term_constituents, row.lhs).words.append(row.param0)
-
-    return term_constituents
-
-
-def create_syntactic_grammar(grammar_df: pd.DataFrame, lexical_grammar: List[Constituent]) -> Grammar:
-    curr_grammar = Grammar()
-    for index, row in grammar_df.iterrows():
-        if row.lhs not in TERMINALS:
-            lhs_const = Constituent(row.lhs, row.lhs in TERMINALS)
-            rhs_const = []
-            for param in COLUMNS[2:]:
-                if not isna(row[param]):
-                    is_terminal_curr = row[param] in TERMINALS
-                    if is_terminal_curr:
-                        curr_const = get_constituent(lexical_grammar, row[param])
-                    else:
-                        curr_const = Constituent(row[param], row[param] in TERMINALS)
-                    rhs_const.append(curr_const)
-
-            current_rule = Rule(lhs_const, rhs_const)
-            curr_grammar.add_rule(current_rule)
-
-    return curr_grammar
+class Early:
+    def __init__(self, grammar: Grammar):
+        self.grammar = grammar
 
 
 if __name__ == "__main__":
-    df_grammar = read_grammar(GRAMMAR_PATH)
-    lexical_grammar = create_lexical_grammar(df_grammar)
-    syntactic_grammar = create_syntactic_grammar(df_grammar, lexical_grammar)
+    grammar = Grammar(GRAMMAR_PATH, TERMINALS, NON_TERMINALS)
+    grammar.create_lexical_grammar()
+    grammar.generate_syntactic_grammar()
--- Trabalho3/grammar.py
+++ Trabalho3/grammar.py
@@ -1,13 +1,58 @@
 from typing import List
 
+import chardet
+import pandas as pd
+
+from Trabalho3.constituent import Constituent
 from rule import Rule
+
+COLUMNS = ["lhs", "sep", "param0", "param1", "param2"]
+
+
+def read_grammar(path: str) -> pd.DataFrame:
+    with open(path, 'rb') as f:
+        enc = chardet.detect(f.read())
+    return pd.read_csv(path, sep='\t', names=COLUMNS, encoding=enc['encoding'])
 
 
 class Grammar:
-    def __init__(self, rules: List[Rule] = None):
+    def __init__(self, grammar_path: str, terminals: List[str], non_terminals: List[str], rules: List[Rule] = None, lexical_rules: List[Constituent] = None):
+        self.non_terminals = non_terminals
+        self.terminals = terminals
         if rules is None:
             rules = []
+        if lexical_rules is None:
+            lexical_rules = []
+        self.dataframe = read_grammar(grammar_path)
         self.rules = rules
+        self.lexical_rules = lexical_rules
 
-    def add_rule(self, rule: Rule):
-        self.rules.append(rule)
+    def get_lexical_rule_by_constituent(self, representation: str) -> Constituent:
+        for const in self.lexical_rules:
+            if const.representation.__eq__(representation):
+                return const
+
+    def create_lexical_grammar(self):
+        for representation in self.terminals:
+            curr_const = Constituent(representation, True)
+            self.lexical_rules.append(curr_const)
+
+        for index, row in self.dataframe.iterrows():
+            if row.lhs in self.terminals:
+                self.get_lexical_rule_by_constituent(row.lhs).words.append(row.param0)
+
+    def generate_syntactic_grammar(self):
+        for index, row in self.dataframe.iterrows():
+            if row.lhs not in self.terminals:
+                lhs_const = Constituent(row.lhs, row.lhs in self.terminals)
+                rhs_const = []
+                for param in COLUMNS[2:]:
+                    if not pd.isna(row[param]):
+                        is_terminal_curr = row[param] in self.terminals
+                        if is_terminal_curr:
+                            curr_const = self.get_lexical_rule_by_constituent(row[param])
+                        else:
+                            curr_const = Constituent(row[param], row[param] in self.terminals)
+                        rhs_const.append(curr_const)
+
+                self.rules.append(Rule(lhs_const, rhs_const))
