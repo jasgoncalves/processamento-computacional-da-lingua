6825e76017bad9fcd5d17ae28b525f7171a35d24
Diogo Fernandes <diogormsf@gmail.com>
2021-12-04 17:32:32 +0000

    Mudada gramática para notação correta e criada gramática sintática a partir da lexical para manter palavras

--- Trabalho3/earley.py
+++ Trabalho3/earley.py
@@ -13,7 +13,7 @@ PROJECT_PATH = os.path.dirname("Trabalho3")
 GRAMMAR_PATH = os.path.join(PROJECT_PATH, "grammar.txt")
 COLUMNS = ["lhs", "sep", "param0", "param1", "param2"]
 NON_TERMINALS = ["S", "NP", "VP", "PP"]
-TERMINALS = ["Art", "N", "Prep", "V", "Adj"]
+TERMINALS = ["Art", "Noun", "Prep", "Verb", "Adj"]
 
 
 def read_grammar(path: str) -> pd.DataFrame:
@@ -21,22 +21,6 @@ def read_grammar(path: str) -> pd.DataFrame:
         enc = chardet.detect(f.read())
     return pd.read_csv(path, sep='\t', names=COLUMNS, encoding=enc['encoding'])
 
-
-def create_syntactic_grammar(grammar_df: pd.DataFrame) -> Grammar:
-    curr_grammar = Grammar()
-    for index, row in grammar_df.iterrows():
-        if row.lhs not in TERMINALS:
-            lhs_const = Constituent(row.lhs, row.lhs in TERMINALS)
-            rhs_const = []
-            for param in COLUMNS[2:]:
-                if not isna(row[param]):
-                    curr_const = Constituent(row[param], row[param] in TERMINALS)
-                    rhs_const.append(curr_const)
-
-            current_rule = Rule(lhs_const, rhs_const)
-            curr_grammar.add_rule(current_rule)
-
-    return curr_grammar
 
 def get_constituent(consts: List[Constituent], representation: str) -> Constituent:
     for const in consts:
@@ -57,7 +41,29 @@ def create_lexical_grammar(grammar_df: pd.DataFrame) -> List[Constituent]:
     return term_constituents
 
 
+def create_syntactic_grammar(grammar_df: pd.DataFrame, lexical_grammar: List[Constituent]) -> Grammar:
+    curr_grammar = Grammar()
+    for index, row in grammar_df.iterrows():
+        if row.lhs not in TERMINALS:
+            lhs_const = Constituent(row.lhs, row.lhs in TERMINALS)
+            rhs_const = []
+            for param in COLUMNS[2:]:
+                if not isna(row[param]):
+                    is_terminal_curr = row[param] in TERMINALS
+                    if is_terminal_curr:
+                        curr_const = get_constituent(lexical_grammar, row[param])
+                    else:
+                        curr_const = Constituent(row[param], row[param] in TERMINALS)
+                    rhs_const.append(curr_const)
+
+            current_rule = Rule(lhs_const, rhs_const)
+            curr_grammar.add_rule(current_rule)
+
+    return curr_grammar
+
+
 if __name__ == "__main__":
     df_grammar = read_grammar(GRAMMAR_PATH)
-    syntactic_grammar = create_syntactic_grammar(df_grammar)
     lexical_grammar = create_lexical_grammar(df_grammar)
+    syntactic_grammar = create_syntactic_grammar(df_grammar, lexical_grammar)
+    print(abc)
