c8c29b526afc31060c3666fd5b536aeaae52428e
Diogo Fernandes <diogormsf@gmail.com>
2021-12-04 17:21:12 +0000

    Criação de gramáticas

--- Trabalho3/constituent.py
+++ Trabalho3/constituent.py
@@ -1,4 +1,10 @@
+from typing import List
+
+
 class Constituent:
-    def __init__(self, representation: str, is_terminal: bool):
+    def __init__(self, representation: str, is_terminal: bool, words: List[str] = None):
+        if words is None:
+            words = []
         self.representation = representation
         self.is_terminal = is_terminal
+        self.words = words
